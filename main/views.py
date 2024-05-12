684672a5173138c1fa77bcffd133a2df1f288c81
T.Leonid <tleonid@gmail.com>
2024-05-12 20:08:46 +0500

    gotove main, pages index about

--- main/views.py
+++ main/views.py
@@ -1,14 +1,20 @@
+from urllib import request
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Привет мир'
+        'title': 'Home - Главная',
     }
 
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('1234567')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'gjljag fjljggj jfljig-4i94jigdfoijggffgfoifsgjghip'
+    }
+
+    return render(request, 'main/about.html', context)
