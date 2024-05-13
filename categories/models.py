005c7895726e734fb50c4e0238bfefd302988364
T.Leonid <tleonid@gmail.com>
2024-05-13 18:56:52 +0500

    :)

--- categories/models.py
+++ categories/models.py
@@ -20,7 +20,7 @@ class Movie(models.Model):
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     grade = models.DecimalField(default=0.0, max_digits=3, decimal_places=2, verbose_name='Оценка')
-    imge = models.ImageField(upload_to='categories_images', blank=True, null=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='categories_images', blank=True, null=True, verbose_name='Изображение')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
     class Meta:
