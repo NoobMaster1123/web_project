70b1c051fe85e5ba97e69ac285aa21dc24e8ad56
T.Leonid <tleonid@gmail.com>
2024-05-14 21:48:28 +0500

    full text search well dun

--- categories/utils.py
+++ categories/utils.py
@@ -1,18 +1,39 @@
 from re import search
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline )
 
 from categories.models import Movie
 
 def q_search(query):
-    
+
     if query.isdigit() and len(query) <= 5:
         return Movie.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Movie.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
+    result = Movie.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+    result = result.annotate(
+        headline = SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">"',
+            stop_sel="</span>"
+        )
+    )
+    result = result.annotate(
+        bodyline = SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">"',
+            stop_sel="</span>"
+        )
+    )
+    return result
 
     # keyword = [word for word in query.split() if len(word) > 2]
     
