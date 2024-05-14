08e1dabdd21f06faeb69434a7278409869cfd7dd
T.Leonid <tleonid@gmail.com>
2024-05-14 20:12:52 +0500

    PostgreSQL data base connected & full text search

--- categories/utils.py
+++ categories/utils.py
@@ -1,4 +1,6 @@
+from re import search
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from categories.models import Movie
 
@@ -7,12 +9,17 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Movie.objects.filter(id=int(query))
     
-    keyword = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Movie.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
+
+    # keyword = [word for word in query.split() if len(word) > 2]
     
-    q_objects = Q()
+    # q_objects = Q()
 
-    for token in keyword:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    # for token in keyword:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
 
-    return Movie.objects.filter(q_objects)
+    # return Movie.objects.filter(q_objects)
